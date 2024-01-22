a1558f9b0f5a4a7b2174d747713f902fedd84914
Shrey <shreyc45@gmail.com>
2024-01-22 08:38:26 -0600

    nasa project files

---
+++ main.js
@@ -0,0 +1,18 @@
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+ 
+  const url = `https://api.nasa.gov/planetary/apod?api_key=0twWZCXn7UPNpwusxxZCehFtNCkaDp8MVRcKUL4N&date=${choice}`
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data.hdurl
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
